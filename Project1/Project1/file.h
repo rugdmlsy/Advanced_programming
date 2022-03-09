cde844c61eda9b394597b909349718c74f02139f
rugdmlsy <1598027117@qq.com>
2022-03-09 22:32:41 +0800

    deleted the old one

--- Project1/Project1/file.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-void update_users();
-void update_goods();
-void update_order();
--- Project1/Project1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "init.h"
-#include "calculator.h"
-#include <iostream>
-#include "user.h"
-#include "admin.h"
-int main()
-{
-	user.sign_up();
-}
